e70cca6d4c7c16df8d0e40f065ed261838def228
lukfd <luca.comba98@gmail.com>
2026-06-07 21:51:04 -0500

    update script.

--- bluestreamer.sh
+++ bluestreamer.sh
@@ -5,37 +5,21 @@ set -euo pipefail
 cmd="${1:-}"
 
 usage() {
-    echo "Usage: $0 [list|start <device_address>|pair <device_address>|trust <device_address>|sink]"
+    echo "Usage: $0 [list|sink|show]"
 }
 
 if [ "$cmd" = "list" ]; then
     bluetoothctl devices
-elif [ "$cmd" = "start" ]; then
-    if [ -z "${2:-}" ]; then
-        usage
-        exit 2
-    fi
-    bluetoothctl power on && bluetoothctl connect "${2}"
-elif [ "$cmd" = "pair" ]; then
-    if [ -z "${2:-}" ]; then
-        usage
-        exit 2
-    fi
-    bluetoothctl power on && bluetoothctl pair "${2}"
-elif [ "$cmd" = "trust" ]; then
-    if [ -z "${2:-}" ]; then
-        usage
-        exit 2
-    fi
-    bluetoothctl trust "${2}"
 elif [ "$cmd" = "sink" ]; then
     bluetoothctl <<'EOF'
 power on
+pairable on
+discoverable on
 agent NoInputNoOutput
 default-agent
-discoverable on
-pairable on
 EOF
+elif [ "$cmd" = "show" ]; then
+    bluetoothctl show
 else
     usage
     exit 2
